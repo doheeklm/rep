ac685c3ea8368b0c58cd9700eebcf333e3d8df16
dhkim <dhkim@telcoware.com>
2022-05-10 15:11:50 +0900

    4-6-2 fn

--- 4-6/my_grep2.c
+++ 4-6/my_grep2.c
@@ -31,6 +31,9 @@ int main(int argc, char *argv[])
 		perror("fopen()");
 	}
 
+	char buffer[BUFSIZE];
+	memset(buffer, 0, sizeof(buffer));
+
 	/* 라인 갯수 확인 */
 	char c;
 	int line = 0;
@@ -40,19 +43,32 @@ int main(int argc, char *argv[])
 		}
 	}
 	printf("[%d 라인 갯수]\n", line);
+	fseek(fp_init, 0, SEEK_SET);
+
+	/* 자식 프로세스 갯수 */
+	int nChild = 0;
+	nChild = (line / LINES_PER_CHILD) + 1;
+	printf("[%d 자식 프로세스 갯수]\n", nChild);
+
+	int a = 0;
+	int cnt = 0;
+	int point[nChild];
+
+	while ((fgets(buffer, sizeof(buffer), fp_init)) != NULL) {
+		if (a % LINES_PER_CHILD == 0) {
+			point[cnt] = (int)ftell(fp_init);
+			printf("%d ", point[cnt]);
+			cnt++;
+		}
+		a++;
+	}
 
 	/* 파일 닫기 */
 	if (fclose(fp_init) != 0) {
 		perror("fclose()");
 	}
 	
-	/* 자식 프로세스 갯수 */
-	int nChild = 0;
-	nChild = (line / LINES_PER_CHILD) + 1;
-	printf("[%d 자식 프로세스 갯수]\n", nChild);
-
-	char filename[100];
-	char buffer[BUFSIZE];
+ 	char filename[50];
 	pid_t pid[nChild];
 
 	int i = 0;
@@ -70,8 +86,13 @@ int main(int argc, char *argv[])
 				perror("fopen()");
 			}
 
-			//fseek(fp, i * LINES_PER_CHILD, SEEK_SET);
-			//fseek의 offset을 바이트로 계산해야하는데...
+			/* 파일 포인터 위치 */
+			if (i == 0) {
+				fseek(fp, 0, SEEK_SET);
+			}
+			else {
+				fseek(fp, point[i], SEEK_SET);
+			}
 
 			FILE* fp_temp;
 			snprintf(filename, sizeof(filename), "./temp%d.txt", i);
@@ -80,20 +101,22 @@ int main(int argc, char *argv[])
 				perror("fopen()");
 			}
 
-			memset(buffer, 0, sizeof(buffer));
 			while (fgets(buffer, sizeof(buffer), fp) != NULL) {
-				printf("fp: %p ", fp);
-				printf("ftell(fp): %ld ", ftell(fp));
-				printf("%s", buffer);
-
 				/* 10줄 이상 읽으면 반복문 중지 */
 				if (lineCheck >= LINES_PER_CHILD) {
 					break;
 				}
 
-				/* 문자열 검색결과  임시 파일에 저장 */
-				if (strstr(buffer, search) != NULL) {
-					fprintf(fp_temp, "%d: %s", lineCheck + (i * LINES_PER_CHILD), buffer);
+				/* 문자열 검색결과 임시 파일에 저장 */
+				if (i == 0) {
+					if (strstr(buffer, search) != NULL) {
+						fprintf(fp_temp, "%d: %s", lineCheck + (i * LINES_PER_CHILD) + 1, buffer);
+					}
+				}
+				else {
+					if (strstr(buffer, search) != NULL) {
+						fprintf(fp_temp, "%d: %s", lineCheck + (i * LINES_PER_CHILD) + 2, buffer);
+					}
 				}
 
 				lineCheck++;
@@ -118,12 +141,14 @@ int main(int argc, char *argv[])
 
 	printf("[%d 프로세스]\n", getpid());
 
-/*	int status = 0;
+	/*
+	int status = 0;
 	while (wait(&status) != -1) {
 		if (WIFEXITED(status) != 0) {
 			printf("정상 종료\n");
 		}
-	};*/
+	};
+	*/
 
 	/* 자식 프로세스 종료 회수 */
 	int status = 0;
@@ -132,9 +157,11 @@ int main(int argc, char *argv[])
 			perror("waitpid()");
 		}
 		if (WIFEXITED(status) != 0) {
-			printf("[%d 자식 프로세스  정상 종료]\n", pid[i]);
+			printf("[%d 자식 프로세스 정상 종료]\n", pid[i]);
 		}
 	}
+
+	
 
 	/* 임시 파일들 읽기 및 삭제 */
 	for (i = 0; i < nChild; i++) {
@@ -144,6 +171,12 @@ int main(int argc, char *argv[])
 		if (fp_final == NULL) {
 			perror("fopen()");
 		}
+
+		struct stat s2;
+		if (stat(filename, &s2) == -1) {
+			perror("stat()");
+		}
+		printf("file size %ld\n", s2.st_size);
 
 		while (feof(fp_final) == 0) {
 			memset(buffer, 0, sizeof(buffer));
