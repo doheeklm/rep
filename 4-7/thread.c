202122da1ff9ac6d5f980270b7ffba0c26737bf1
dhkim <dhkim@telcoware.com>
2022-05-13 13:42:55 +0900

    4-7

--- 4-7/thread.c
+++ 4-7/thread.c
@@ -1,37 +1,40 @@
 #include <stdio.h>
 #include <unistd.h> //access()
-#include <pthread.h> //pthread..
+#include <pthread.h> //pthread_create(), pthread_join()
 #include <stdlib.h> //exit()
-#include <string.h>
+#include <sys/stat.h> //stat()
+#include <string.h> //strstr()
 #include <errno.h>
 
 enum { LINES_PER_CHILD = 20, BUFSIZE = 255 };
 
-char* path;
 char* search;
-int i;
+char* path;
+
+typedef struct Info
+{
+	int cnt;
+	int ptr;
+} Info;
 
 void *t_function(void *data)
 {
-	/*	int j = 0;
-	while (j < 3) {
-		printf("no.%d 쓰레드 : %d\n", info.num, j);
-		j++;
-		sleep(1);
-	}*/
- 
-	i = *((int *)data);
+	Info *temp = (Info *)data;
+
+	char filename[50];
+	snprintf(filename, sizeof(filename), "./temp%d.txt", temp->cnt);
+
+	printf("Thread #%d\n", temp->cnt);
+	printf("Searching %s in %s [%d~]\n", search, path, temp->ptr);
+	printf("Saving in %s..\n", filename);
+	printf("=============================\n");
 
 	FILE* fp;
-	fp = fopen(path, "r'");
+	fp = fopen(path, "r");
 	if (fp == NULL) {
 		perror("fopen()");
 	}
-		
-	fseek(fp, 0, SEEK_SET);
-
-	char filename[50];
-	snprintf(filename, sizeof(filename), "./temp%d.txt", 0);
+	fseek(fp, temp->ptr, SEEK_SET);
 
 	FILE* fp_temp;
 	fp_temp = fopen(filename, "w+");
@@ -40,16 +43,14 @@ void *t_function(void *data)
 	}
 
 	char buf[BUFSIZE];
-
 	int lineCheck = 1;
 	while (fgets(buf, sizeof(buf), fp) != NULL) {
 		if (lineCheck > LINES_PER_CHILD) {
 			break;
 		}
-		if (strstr(buf, search) != NULL) {			
-			printf("%d:", lineCheck + (i * LINES_PER_CHILD));
-			printf("%s", buf);
-			fprintf(fp_temp, "%d:%s", lineCheck + (i * LINES_PER_CHILD), buf);
+		if (strstr(buf, search) != NULL) {
+			//printf("%d:%s", lineCheck + (temp->cnt * LINES_PER_CHILD), buf);
+			fprintf(fp_temp, "%d:%s", lineCheck + (temp->cnt * LINES_PER_CHILD), buf);
 		}
 		lineCheck++;
 	}
@@ -60,7 +61,7 @@ void *t_function(void *data)
 	if (fclose(fp_temp) != 0) {
 		perror("fclose()");
 	}
-
+	
 	return NULL;
 }
 
@@ -76,15 +77,18 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
+	/* 인자로 받은 문자열 & 파일 */
 	search = argv[1];
 	path = argv[2];
 
+	/* 라인 갯수, 스레드 갯수, 파일의 포인터 위치 확인 위해 파일 열기 */
 	FILE* fp_init;
 	fp_init = fopen(argv[2], "r");
 	if (fp_init == NULL) {
 		perror("fopen()");
 	}
 
+	/* 라인 갯수 */
 	char c;
 	int line = 0;
 	while ((c = fgetc(fp_init)) != EOF) {
@@ -92,30 +96,35 @@ int main(int argc, char *argv[])
 			line++;
 		}
 	}
-	printf("[%d 라인 갯수]\n", line);
+	//printf("[%d 라인 갯수]\n", line);
+
+	/* 파일 돌렸으니 위치 맨앞으로 변경 */
 	fseek(fp_init, 0, SEEK_SET);
 
+	/* 스레드 갯수 */
 	int nThread = 0;
 	nThread = (line / LINES_PER_CHILD) + 1;
-	printf("[%d Thread 갯수]\n", nThread);
+	//printf("[%d 스레드 갯수]\n", nThread);
 
+	int i = 0;
 	int a = 0;
 	int b = 0;
-	int point[nThread];
+	int point[nThread]; //포인터 위치
 
 	char buffer[BUFSIZE];
 	memset(buffer, 0, sizeof(buffer));
 
+	/* 라인 n줄(LINES_PER_CHILD)마다 포인터 위치 확인, point[]에 저장 */
 	while (fgets(buffer, sizeof(buffer), fp_init) != NULL) {
 		if (a == 0) {
 			point[b] = 0;
-			printf("%d줄 파일포인터 %d\n", a, point[b]);
+			//printf("%d줄 파일포인터 %d\n", a, point[b]);
 			b++;
 		}
 		else {
 			if (a % LINES_PER_CHILD == LINES_PER_CHILD - 1) {
 				point[b] = (int)ftell(fp_init);
-				printf("%d줄 파일포인터 %d\n", a, point[b]);
+				//printf("%d줄 파일포인터 %d\n", a, point[b]);
 				b++;
 			}
 		}
@@ -126,36 +135,68 @@ int main(int argc, char *argv[])
 		perror("fclose()");
 	}
 
+	/* 스레드 */
 	pthread_t p_thread[nThread];
 
-	printf("Create Thread\n");
+	/* 몇번째 스레드인지, 스레드 내 파일 위치 정보 */
+	Info info[nThread];
 
+	/* 스레드 nThread개 생성 */
 	for (i = 0; i < nThread; i++) {
-		if (pthread_create(&p_thread[i], NULL, t_function, (void *)&i) != 0) {
+		info[i].cnt = i;
+		info[i].ptr = point[i];
+
+		if (pthread_create(&p_thread[i], NULL, t_function, (void *)&info[i]) != 0) {
 			perror("pthread_create()");
 			exit(EXIT_FAILURE);
 		}
 	}
 
-
-	/*	for (i = 0; i < nThread; i++) {
-		//예외처리
-		pthread_detach(p_thread[i]);
-		pause();
+	/* 각 스레드 종료를 기다림 */
+	for (i = 0; i < nThread; i++) {
+		if (pthread_join(p_thread[i], NULL) != 0) {
+			perror("pthread_join()");
+			exit(EXIT_FAILURE);
+		}
 	}
-*/
+	
+	char name[50];
+	int fileSize;
 
-	int j;
-	for (j = 0; j < nThread; j++) {
-		pthread_join(p_thread[j], NULL);
+	/* 최종 결과 화면 출력 및 임시 파일들 삭제 */
+	for (i = 0; i < nThread; i++) {
+		snprintf(name, sizeof(name), "./temp%d.txt", i);
+		
+		FILE* fp_final;
+		fp_final = fopen(name, "r");
+		if (fp_final == NULL) {
+			perror("fopen()");
+			exit(EXIT_FAILURE);
+		}
+
+		struct stat s2;
+		if (stat(name, &s2) == -1) {
+			perror("stat()");
+			exit(EXIT_FAILURE);
+		}
+		fileSize = (int)s2.st_size;
+		fileSize += fileSize;
+
+		while (fgets(buffer, sizeof(buffer), fp_final) != NULL) {
+			printf("%s", buffer);
+		}
+
+		if (fclose(fp_final) != 0) {
+			perror("fclose()");
+			exit(EXIT_FAILURE);
+		}
+
+		unlink(name);
 	}
 
-//	for (i = 0; i < nThread; i++) {
-//		if (pthread_join(p_thread[i], (void **)&status) == 0) {
-
-//		}
-//		printf("%d status %d\n", i, status);
-//	}
+	if (fileSize == 0) {
+		printf("\"%s\" not found in %s\n", search, path);
+	}
 
 	return 0; 
 }
