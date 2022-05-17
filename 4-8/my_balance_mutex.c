95abb7d44519ff4b7a5ddd6b1d74e31e529eb4e3
dhkim <dhkim@telcoware.com>
2022-05-17 16:17:28 +0900

    4-8-1 final

--- 4-8/my_balance_mutex.c
+++ 4-8/my_balance_mutex.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h> //exit() malloc() free()
-#include <errno.h> //errno
 #include <string.h> //strerror_r()
 #include <unistd.h> //sleep()
 
 enum { ERR_BUFSIZE = 64, NUM_THREAD = 5, MAX_BALANCE = 10000,
 		OUT_OF_RANGE = 0, IN_RANGE = -1 };
 
-char errmsg[ERR_BUFSIZE];
-
 static int balance;
 static pthread_mutex_t mutex;
 
 typedef struct DATA{
-	int cnt;
+	int count;
 	int time;
 	int amount;
 } DATA;
@@ -25,8 +22,6 @@ int OffLimits()
 		return IN_RANGE;
 	}
 
-	printf("잔고가 10,000원을 초과했습니다. 프로그램 종료\n");
-
 	return OUT_OF_RANGE;
 }
 
@@ -35,44 +30,35 @@ void *Balance(void *data)
 	DATA d = *(DATA *)data;
 	
 	while (balance < MAX_BALANCE) {
-		if (pthread_mutex_lock(&mutex) != 0) {
-			if (strerror_r(errno, errmsg, ERR_BUFSIZE) != 0) {
-				fprintf(stderr, "errno[%d] : %s\n", errno, errmsg);
-				exit(EXIT_FAILURE);
-			}	
-		}
-	
-#if DEBUG
-		printf("스레드 [%d]\n", d.cnt);
-#endif
 
-		if (d.cnt == 3 || d.cnt == 4) { //잔고 증가&감소시키는 스레드 2개
+		if (pthread_mutex_lock(&mutex) != 0) {
+			perror("mutex lock");
+			return NULL;
+		}
+
+		if (d.count == 3 || d.count == 4) {
 			balance += d.amount;
-#if DEBUG
 			if (d.amount > 0) {
-				printf("+%d\n", d.amount);
+				printf("스레드[%d] +%d\n", d.count, d.amount);
 			}
 			else if (d.amount < 0) {
-				printf("%d\n", d.amount);
+				printf("스레드[%d] %d\n", d.count, d.amount);
 			}
-#endif
 		}
-		else { //잔고 출력하는 스레드 3개
-			printf("%d원\n", balance);
-		}
-		
-		if (pthread_mutex_unlock(&mutex) != 0) {
-			if (strerror_r(errno, errmsg, ERR_BUFSIZE) != 0) {
-				fprintf(stderr, "errno[%d] : %s\n", errno, errmsg);
-				exit(EXIT_FAILURE);
-			}
+		else {
+			printf("스레드[%d] %d원\n", d.count, balance);
 		}
 	
+		if (pthread_mutex_unlock(&mutex) != 0) {
+			perror("mutex unlock");
+			return NULL;
+		}
+
 		sleep(d.time);
 	}
-
+	
 	if (OffLimits() == OUT_OF_RANGE) {
-		exit(EXIT_SUCCESS);
+		return NULL;
 	}
 
 	return NULL;
@@ -89,14 +75,13 @@ int main()
 	data = (struct DATA *)malloc(sizeof(struct DATA) * NUM_THREAD);
 
 	if (pthread_mutex_init(&mutex, NULL) != 0) {
-		if (strerror_r(errno, errmsg, ERR_BUFSIZE) != 0) {
-			fprintf(stderr, "errno[%d] : %s\n", errno, errmsg);
-			exit(EXIT_FAILURE);
-		}		
+		perror("mutex init");
+		free(data);
+		exit(EXIT_FAILURE);
 	}
 
 	for (i = 0; i < NUM_THREAD; i++) {
-		data[i].cnt = i;
+		data[i].count = i;
 		data[i].time = 1;
 		data[i].amount = 0;
 
@@ -109,27 +94,24 @@ int main()
 		}
 		
 		if (pthread_create(&p_thread[i], NULL, Balance, (void *)&data[i]) != 0) {
-			if (strerror_r(errno, errmsg, ERR_BUFSIZE) != 0) {
-				fprintf(stderr, "errno[%d] : %s\n", errno, errmsg);
-				exit(EXIT_FAILURE);
-			}		
+			perror("thread create");
+			free(data);
+			exit(EXIT_FAILURE);
 		}
 	}
 
 	for (i = 0; i < NUM_THREAD; i++) {
 		if (pthread_join(p_thread[i], (void **)NULL) != 0) {
-			if (strerror_r(errno, errmsg, ERR_BUFSIZE) != 0) {
-				fprintf(stderr, "errno[%d] : %s\n", errno, errmsg);
-				exit(EXIT_FAILURE);
-			}
+			perror("thread join");
+			free(data);
+			exit(EXIT_FAILURE);
 		}
 	}
 	
 	if (pthread_mutex_destroy(&mutex) != 0) {
-		if (strerror_r(errno, errmsg, ERR_BUFSIZE) != 0) {
-			fprintf(stderr, "errno[%d] : %s\n", errno, errmsg);
-			exit(EXIT_FAILURE);
-		}
+		perror("mutex destroy");
+		free(data);
+		exit(EXIT_FAILURE);
 	}
 
 	free(data);
