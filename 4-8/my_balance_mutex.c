1f529b693014a036652265aed9a455f343920594
dhkim <dhkim@telcoware.com>
2022-05-18 17:35:17 +0900

    4-8 final

--- 4-8/my_balance_mutex.c
+++ 4-8/my_balance_mutex.c
@@ -3,32 +3,29 @@
 #include <stdlib.h> //exit() malloc() free()
 #include <errno.h> //errno
 #include <string.h> //strerror_r()
-#include <unistd.h> //sleep()
 #include <time.h> //nanosleep() clock_gettime()
 
 enum { ERR_BUFSIZE = 64, NUM_CHECK = 3, NUM_UPDATE = 2,
-	MAX_BALANCE = 10000, OUT_OF_RANGE = 0, IN_RANGE = -1 };
+	MAX_BALANCE = 10000 };
 
 static int balance;
 static pthread_mutex_t mutex;
 
 typedef struct DATA{
-	int count;
-	int time;
-	int amount;
+	int count; //스레드 번호
+	int amount; //증감할 돈
 } DATA;
 
 void *Check(void *data);
 void *Update(void *data);
-int BalanceRange();
 
 int main()
 {
 	size_t i;
 	balance = 0;
 
-	pthread_t th_check[NUM_CHECK];
-	pthread_t th_update[NUM_UPDATE];
+	pthread_t th_check[NUM_CHECK]; //잔고 출력하는 스레드
+	pthread_t th_update[NUM_UPDATE]; //잔고 업데이트하는 스레드
 
 	DATA *data_ch;
 	data_ch = (struct DATA *)malloc(sizeof(struct DATA) * NUM_CHECK);
@@ -43,8 +40,7 @@ int main()
 	}
 
 	for (i = 0; i < NUM_CHECK; i++) {
-		data_ch[i].count = i;
-		data_ch[i].time = 1;
+		data_ch[i].count = i; //스레드 0, 1, 2번
 		data_ch[i].amount = 0;
 		
 		if (pthread_create(&th_check[i], NULL, Check, (void *)&data_ch[i]) != 0) {
@@ -56,15 +52,13 @@ int main()
 	}
 
 	for (i = 0; i < NUM_UPDATE; i++) {
-		data_up[i].count = i + NUM_CHECK;
+		data_up[i].count = i + NUM_CHECK; //스레드 3, 4번
 		
 		if (i == 0) {
-			data_up[i].time = 1;
-			data_up[i].amount = 1000;
+			data_up[i].amount = 1000; //1초에 1000원씩 증가
 		}
 		else if (i == 1) {
-			data_up[i].time = 2;
-			data_up[i].amount = -500;
+			data_up[i].amount = -500; //2초에 500원씩 감소
 		}
 
 		if (pthread_create(&th_update[i], NULL, Update, (void *)&data_up[i]) != 0) {
@@ -75,7 +69,6 @@ int main()
 		}
 	}
 
-	//메인 스레드는 은행 잔고가 10,000원 이상이 되면 모든 스레드를 종료하고 프로그램을 종료함
 	for (i = 0; i < NUM_CHECK; i++) {
 		if (pthread_join(th_check[i], (void **)NULL) != 0) {
 			free(data_ch);
@@ -93,7 +86,7 @@ int main()
 			exit(EXIT_FAILURE);
 		}
 	}
-
+	
 	if (pthread_mutex_destroy(&mutex) != 0) {
 		free(data_ch);
 		free(data_up);
@@ -109,154 +102,133 @@ int main()
 
 void *Check(void *data)
 {
-	int delta = 0;
+	long deltaTime = 0;
 
-	struct timespec start = {0, 0};
-	struct timespec end = {0, 0};
-
-	DATA d = *(DATA *)data;
-		
-	while (balance < MAX_BALANCE) {
-		if (clock_gettime(CLOCK_REALTIME, &start) != 0) {
-			fprintf(stderr, "errno[%d]", errno);
-			return NULL;
-		}
-
-		struct timespec reqtime;
-		reqtime.tv_sec = 0;
-		reqtime.tv_nsec = 999999999;
-		if (nanosleep(&reqtime, NULL) != 0) {
-			fprintf(stderr, "errno[%d]", errno);
-			return NULL;
-		}
-
-		if (clock_gettime(CLOCK_REALTIME, &end) != 0) {
-			fprintf(stderr, "errno[%d]", errno);
-			return NULL;
-		}
-
-		delta = (end.tv_sec - start.tv_sec) + ((end.tv_nsec - start.tv_nsec) / 1000000000);
-		
-		if (BalanceRange() == OUT_OF_RANGE) {
-			return NULL;
-		}
-
-		if (delta >= 1) {
-			if (pthread_mutex_lock(&mutex) != 0) {
-				fprintf(stderr, "errno[%d]", errno);
-				return NULL;
-			}
-			
-			printf("스레드[%d] %d원\n", d.count, balance);
-
-			if (pthread_mutex_unlock(&mutex) != 0) {
-				fprintf(stderr, "errno[%d]", errno);
-				return NULL;
-			}		
-		}
-
-	//	if (d.count == 2) {
-	//		sleep(20);	
-	//	}
-	//	else {
-	//		sleep(d.time);
-	//	}
-
-	}
-
-	return NULL;	
-}
-
-void *Update(void *data)
-{
-	int delta = 0;
-
-	struct timespec start = {0, 0};
-	struct timespec end = {0, 0};
+	struct timespec start = { 0, 0 };
+	struct timespec end = { 0, 0 };
+	struct timespec sleep = { 0, 010000000 }; //sec:0, nsec:01 => 0.01초
 
 	DATA d = *(DATA *)data;
-	
-	while (balance < MAX_BALANCE) {
-		if (clock_gettime(CLOCK_REALTIME, &start) != 0) {
-			fprintf(stderr, "errno[%d]", errno);
-			return NULL;
-		}
-	
-		struct timespec reqtime;
-		if (d.amount > 0) {
-			reqtime.tv_sec = 0;
-		}
-		else if (d.amount < 0) {
-			reqtime.tv_sec = 1;
-		}
-		reqtime.tv_nsec = 999999999;
-		if (nanosleep(&reqtime, NULL) != 0) {
+
+	while (1) {
+		if (clock_gettime(CLOCK_MONOTONIC, &start) != 0) {
 			fprintf(stderr, "errno[%d]", errno);
 			return NULL;
 		}
 
-		if (clock_gettime(CLOCK_REALTIME, &end) != 0) {
-			fprintf(stderr, "errno[%d]", errno);
-			return NULL;
-		}
-
-		delta = (end.tv_sec - start.tv_sec) + ((end.tv_nsec - start.tv_nsec) / 1000000000);
-
-		if (BalanceRange() == OUT_OF_RANGE) {
-			return NULL;
-		}
-
-		if (d.amount > 0 && delta >= 1) {
-			if (pthread_mutex_lock(&mutex) != 0) {
+		while (1) {
+			if (clock_gettime(CLOCK_MONOTONIC, &end) != 0) {
 				fprintf(stderr, "errno[%d]", errno);
 				return NULL;
 			}
 
-			balance += d.amount;
+			deltaTime = end.tv_sec - start.tv_sec;
+			//printf("delta %ld\n", deltaTime);
+			if (deltaTime >= 1) {
+				break;
+			}
 
-			if (d.amount > 0) {
-				printf("스레드[%d] +%d\n", d.count, d.amount);
+			if (nanosleep(&sleep, NULL) != 0) {
+				fprintf(stderr, "errno[%d]", errno);
+				return NULL;
 			}
-			else if (d.amount < 0) {
-				printf("스레드[%d] %d\n", d.count, d.amount);
-			}
+		}
+		
+		//////////* mutex lock *//////////
+		if (pthread_mutex_lock(&mutex) != 0) {
+			fprintf(stderr, "errno[%d]", errno);
+			return NULL;
+		}
 	
+		if (balance >= MAX_BALANCE) {
+			printf("알림: 스레드 [%d] - 잔고 10000 이상. 프로그램 종료\n", d.count);
 			if (pthread_mutex_unlock(&mutex) != 0) {
 				fprintf(stderr, "errno[%d]", errno);
 				return NULL;
 			}
-		}
-		else if (d.amount < 0 && delta >= 2) {
-			if (pthread_mutex_lock(&mutex) != 0) {
-				fprintf(stderr, "errno[%d]", errno);
-				return NULL;
-			}
-
-			balance += d.amount;
-
-			if (d.amount > 0) {
-				printf("스레드[%d] +%d\n", d.count, d.amount);
-			}
-			else if (d.amount < 0) {
-				printf("스레드[%d] %d\n", d.count, d.amount);
-			}
-	
-			if (pthread_mutex_unlock(&mutex) != 0) {
-				fprintf(stderr, "errno[%d]", errno);
-				return NULL;
-			}
+			return NULL;
 		}
 
-//		sleep(d.time);
+		printf("스레드[%d] %d원\n", d.count, balance);
+
+		if (pthread_mutex_unlock(&mutex) != 0) {
+			fprintf(stderr, "errno[%d]", errno);
+			return NULL;
+		}
+		///////////////////////////////////
 	}
 
 	return NULL;
 }
 
-int BalanceRange()
+void *Update(void *data)
 {
-	if (balance < MAX_BALANCE) {
-		return IN_RANGE; //-1
+	int deltaTime = 0;
+
+	struct timespec start = { 0, 0 };
+	struct timespec end = { 0, 0 };
+	struct timespec sleep = { 0, 010000000 }; //sec:0, nsec:01 => 0.01초
+
+	DATA d = *(DATA *)data;
+
+	while (1) {
+		if (clock_gettime(CLOCK_MONOTONIC, &start) != 0) {
+			fprintf(stderr, "errno[%d]", errno);
+			return NULL;
+		}
+
+		while (1) {
+			if (clock_gettime(CLOCK_MONOTONIC, &end) != 0) {
+				fprintf(stderr, "errno[%d]", errno);
+				return NULL;
+			}
+
+			deltaTime = end.tv_sec - start.tv_sec;
+			//printf("delta %ld\n", deltaTime);
+
+			if (d.amount < 0 && deltaTime >= 2) {
+				break;
+			}
+			else if (d.amount > 0 && deltaTime >= 1) {
+				break;
+			}
+
+			if (nanosleep(&sleep, NULL) != 0) {
+				fprintf(stderr, "errno[%d]", errno);
+				return NULL;
+			}
+		}
+		
+		//////////* mutex lock *//////////
+		if (pthread_mutex_lock(&mutex) != 0) {
+			fprintf(stderr, "errno[%d]", errno);
+			return NULL;
+		}
+
+		if (balance >= MAX_BALANCE) {
+			printf("알림: 스레드 [%d] - 잔고 10000 이상. 프로그램 종료\n", d.count);
+			if (pthread_mutex_unlock(&mutex) != 0) {
+				fprintf(stderr, "errno[%d]", errno);
+				return NULL;
+			}
+			return NULL;
+		}
+
+		balance += d.amount;
+
+		if (d.amount > 0) {
+			printf("스레드[%d] +%d\n", d.count, d.amount);
+		}
+		else if (d.amount < 0) {
+			printf("스레드[%d] %d\n", d.count, d.amount);
+		}
+
+		if (pthread_mutex_unlock(&mutex) != 0) {
+			fprintf(stderr, "errno[%d]", errno);
+			return NULL;
+		}
+		///////////////////////////////////
 	}
-	return OUT_OF_RANGE; //0
+
+	return NULL;
 }
