8097a41f58cf9ed841a13f05146a88309e12e119
dhkim <dhkim@telcoware.com>
2022-06-09 08:49:40 +0900

    4-10 snd

--- 4-10/snd.c
+++ 4-10/snd.c
@@ -12,10 +12,8 @@
 #include <stdio_ext.h> //__fpurge()
 #include <unistd.h> //getpid()
 
-//pthread_mutex_t mutex; //뮤텍스 객체 변수
-//static sem_t sem; //세마포어 객체 변수
-
-//enum { EMPTY = 0, NOT_EMPTY = -1 };
+pthread_mutex_t mutex; //뮤텍스 객체
+static sem_t sem; //세마포어 정수 변수
 
 typedef struct _MSG {
 	/* 메시지 타입 */
@@ -32,34 +30,78 @@ void ClearStdin(char* c); //버퍼 삭제
 int main()
 {
 	Msg msg;
-	memset(&msg, 0, sizeof(msg));
-
-	msg.mtype = getpid();
+//	msg.mtype = getpid();
 
 	int msg_size = 0;
 	msg_size = sizeof(msg) - sizeof(msg.mtype);
 
-	//이름 입력받기
-	printf("Name: ");
-	if(fgets(msg.name, sizeof(msg.name), stdin) == NULL) {
+	//세마포어 초기화
+	if (sem_init(&sem, 0, 0) != 0) {
 		fprintf(stderr, "errno[%d]", errno);
+		exit(EXIT_FAILURE);
 	}
-	ClearStdin(msg.name);
 
-	//전화번호 입력받기
-	printf("Phone Num: ");
-	if (fgets(msg.phone, sizeof(msg.phone), stdin) == NULL) {
+	//뮤텍스 초기화
+	if (pthread_mutex_init(&mutex, NULL) != 0) {
+		if (sem_destroy(&sem) != 0) {
+			fprintf(stderr, "errno[%d]", errno);
+		}
 		fprintf(stderr, "errno[%d]", errno);
+		exit(EXIT_FAILURE);
 	}
-	ClearStdin(msg.phone);
+
+	do {
+		//뮤텍스 락
+		if (pthread_mutex_lock(&mutex) != 0) {
+			fprintf(stderr, "errno[%d]", errno);
+			goto EXIT;
+		}
+	
+		memset(&msg, 0, sizeof(msg));
+
+		//이름 입력받기
+		printf("Name: ");
+		if(fgets(msg.name, sizeof(msg.name), stdin) == NULL) {
+			fprintf(stderr, "errno[%d]", errno);
+			goto EXIT;
+		}
+		ClearStdin(msg.name);
+
+		//exit 입력받으면 프로그램 종료
+		if (strcmp(str_exit, msg.name) == 0) {
+			printf("입력을 종료합니다.\n");
+			goto EXIT;
+		}
+
+		//전화번호 입력받기
+		printf("Phone Num: ");
+		if (fgets(msg.phone, sizeof(msg.phone), stdin) == NULL) {
+			fprintf(stderr, "errno[%d]", errno);
+			goto EXIT;
+		}
+		ClearStdin(msg.phone);
+
+		//입력한 전화번호 형태가 잘못된 경우, 에러 문구를 출력하고 이름부터 다시 입력 받음
+		if ((msg.phone[3] != '-') || (msg.phone[8] != '-')) {
+			printf("전화번호를 xxx-xxxx-xxxx 형태로 입력해주세요.\n");
+			printf("처음으로 돌아갑니다.\n");
+		}
+	} while ((msg.phone[3] != '-') || (msg.phone[8] != '-'));
 
 	//주소 입력받기
 	printf("Address: ");
 	if (fgets(msg.address, sizeof(msg.address), stdin) == NULL) {
 		fprintf(stderr, "errno[%d]", errno);
+		goto EXIT;
 	}
 	ClearStdin(msg.address);
 	
+	//뮤텍스 언락
+	if (pthread_mutex_unlock(&mutex) != 0) {
+		fprintf(stderr, "errno[%d]", errno);
+		goto EXIT;
+	}
+
 	//키 생성
 	key_t key = 0;
 	if ((key = ftok(".", 'A')) == -1) {
@@ -67,10 +109,27 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
-	int qid = 0;
+	int qid = 0; //메세지 큐 식별자
 	qid = msgget(key, IPC_CREAT | 0666);
 
 	if (msgsnd(qid, (void *)&msg, msg_size, IPC_NOWAIT) == -1) {
+		fprintf(stderr, "errno[%d]", errno);
+	}
+
+	//세마포어 값 1 증가
+	if (sem_post(&sem) != 0) {
+		fprintf(stderr, "errno[%d]", errno);
+		goto EXIT;
+	}
+
+EXIT:
+	//뮤텍스 소멸
+	if (pthread_mutex_destroy(&mutex) != 0) {
+		fprintf(stderr, "errno[%d]", errno);
+	}
+
+	//세마포어 제거
+	if (sem_destroy(&sem) != 0) {
 		fprintf(stderr, "errno[%d]", errno);
 	}
 
