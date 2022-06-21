82f329bdf771555d1689290e1d94aab0fef4b01d
dhkim <dhkim@telcoware.com>
2022-06-21 13:03:33 +0900

    4-11

--- 4-11/tcp_client.c
+++ 4-11/tcp_client.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <stdio_ext.h>
 #include <sys/types.h> //socket() connect()
 #include <sys/socket.h> //socket() connect() inet_addr()
 #include <unistd.h> //read() write() close()
@@ -10,7 +11,6 @@
 #include <netinet/in.h> //inet_addr()
 
 #define PORT 7777
-#define MAX_PENDING 5
 
 typedef struct _Data {
 	char name[13];
@@ -18,40 +18,106 @@ typedef struct _Data {
 	char address[151];
 } Data; 
 
+const char* str_exit = "exit\n";
 void ClearStdin(char* c);
 
-int main(int argc, char *argv[])
+int main()
 {
-//	const char* str_exit = "exit";
-	
-//	Data data;
-	
-	int sockfd = 0;
+	Data data;
 
+	int cSockFd = 0;
 	struct sockaddr_in sAddr;
-	socklen_t sAddrSize = 0;
-	sAddrSize = sizeof(sAddr);
+	socklen_t sAddrSize = sizeof(sAddr);
 
-	if ((sockfd = (socket(PF_INET, SOCK_STREAM, 0))) == -1) {
-		fprintf(stderr, "socket/errno[%d]", errno);
-		//return
+	//소켓 생성
+	if ((cSockFd = (socket(PF_INET, SOCK_STREAM, 0))) == -1) {
+		fprintf(stderr, "socket|errno[%d]", errno);
+		return 0;
 	}
 
 	memset(&sAddr, 0, sizeof(sAddr));
 	sAddr.sin_family = AF_INET;
-	sAddr.sin_addr.s_addr = inet_addr(argv[1]);
 	sAddr.sin_port = htons(PORT);
+	if (sAddr.sin_port == -1) {
+		fprintf(stderr, "htons|errno[%d]", errno);
+		goto EXIT;
+	}
+	sAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (sAddr.sin_addr.s_addr == -1) {
+		fprintf(stderr, "htonl|errno[%d]", errno);
+		goto EXIT;
+	}
 
-	if (connect(sockfd, (struct sockaddr*)&sAddr, sAddrSize) == -1) {
-		fprintf(stderr, "connect/errno[%d]", errno);
-		//return
+	//서버로 연결 시도
+	if (connect(cSockFd, (struct sockaddr*)&sAddr, sAddrSize) == -1) {
+		fprintf(stderr, "connect|errno[%d]", errno);
+		goto EXIT;
+	}
+		
+	while (1) {
+		memset(&data, 0, sizeof(data));
+
+		do {
+			printf("Name: ");
+			if (fgets(data.name, sizeof(data.name), stdin) == NULL) {
+				fprintf(stderr, "fgets|errno[%d]", errno);
+				goto EXIT;
+			}
+			ClearStdin(data.name);
+
+			if (strcmp(str_exit, data.name) == 0) {
+				goto EXIT;
+			}
+
+			printf("Phone Num: ");
+			if (fgets(data.phone, sizeof(data.phone), stdin) == NULL) {
+				fprintf(stderr, "fgets|errno[%d]", errno);
+				goto EXIT;
+			}
+			ClearStdin(data.phone);
+
+			if ((data.phone[3] != '-') || (data.phone[8] != '-')) {
+				printf("전화번호를 xxx-xxxx-xxxx 형태로 입력해주세요.\n");
+				printf("처음으로 돌아갑니다.\n");
+			}
+		} while ((data.phone[3] != '-') || (data.phone[8] != '-'));
+
+		printf("Address: ");
+		if (fgets(data.address, sizeof(data.address), stdin) == NULL) {
+			fprintf(stderr, "fgets|errno[%d]", errno);
+			goto EXIT;
+		}
+		ClearStdin(data.address);
+
+		printf("%s/%s/%s", data.name, data.phone, data.address);
+
+		if (write(cSockFd, &data, sizeof(data)) == -1) {
+			fprintf(stderr, "write|errno[%d]", errno);
+			goto EXIT;
+		}
+	}
+
+EXIT:
+	if (shutdown(cSockFd, SHUT_RDWR) == -1) {
+		fprintf(stderr, "shutdown|errno[%d]", errno);
+	}
+
+	if (close(cSockFd) == -1) {
+		fprintf(stderr, "close|errno[%d]", errno);	
 	}
 	
-	char msg[30];
-	read(sockfd, msg, sizeof(msg) - 1);
-	printf("Msg: %s\n", msg);
-
-	close(sockfd);
-
 	return 0;
+}
+
+void ClearStdin(char* c)
+{
+	if (c == NULL) {
+		return;
+	}
+
+	if (c[strlen(c) - 1] == '\n') {
+		c[strlen(c) - 1] == '\0';
+	}
+
+	__fpurge(stdin);
 }
--- 4-11/tcp_server.c
+++ 4-11/tcp_server.c
@@ -8,8 +8,8 @@
 #include <unistd.h> //read() write() close()
 #include <arpa/inet.h>
 
-#define PORT 7777
-#define MAXPENDING 5
+#define PORT 7777 //포트 번호
+#define MAX_PENDING 5 //연결요청 대기 큐의 크기
 
 typedef struct _DATA {
 	char name[13];
@@ -21,11 +21,12 @@ int main()
 {
 	Data data;
 
-	int sSockFd;
-	int cSockFd;
+	int sSockFd; //서버 소켓
+	int cSockFd; //클라이언트 소켓
+	int Bind; //bind 성공여부
 	
-	struct sockaddr_in sAddr;
-	struct sockaddr_in cAddr;
+	struct sockaddr_in sAddr; //서버의 주소 정보 
+	struct sockaddr_in cAddr; //클라이언트의 주소 정보 
 	
 	socklen_t sAddrSize = 0;
 	socklen_t cAddrSize = 0;
@@ -33,59 +34,88 @@ int main()
 	sAddrSize = sizeof(sAddr);
 	cAddrSize = sizeof(cAddr);
 
-	if ((sSockFd == socket(PF_INET, SOCK_STREAM, 0)) == -1) {
-		fprintf(stderr, "socket()_errno[%d]:%s\n", errno, strerror(errno));
+	//소켓 생성
+	sSockFd = socket(PF_INET, SOCK_STREAM, 0);
+	if (sSockFd == -1) {
+		fprintf(stderr, "socket|errno[%d]", errno);
 		return 0;
 	}
 
-	if (setsockopt(sSockFd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
-		perror("setsockopt");
-		exit(1);
-	}
-
-	memset(&sAddr, 0, sizeof(sAddr));
+	memset(&sAddr, 0, sAddrSize);
 	sAddr.sin_family = AF_INET; //IPv4 인터넷 프로토콜
-	sAddr.sin_port = htons(PORT); //사용할 PORT 번호
-	sAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	sAddr.sin_port = htons(PORT); //사용할 포트 저장 
+	if (sAddr.sin_port == -1) {
+		fprintf(stderr, "htons|errno[%d]", errno);
+		goto EXIT;
+	}
+	sAddr.sin_addr.s_addr = htonl(INADDR_ANY); //IP주소 자동으로 할당
+	if (sAddr.sin_addr.s_addr == -1) {
+		fprintf(stderr, "htonl|errno[%d]", errno);
+		goto EXIT;
+	}
+	//INADDR_ANY :
+	//IP주소가 하나인 컴퓨터에서는 현재 컴퓨터의 IP주소를 소켓에 할당하며,
+	//Multi-homed(IP가 2개 이상)인 경우, 어떤 IP로 데이터가 들어오든지
+	//포트 번호만 맞으면 해당 포트로 데이터를 전달해주도록 하는 상수. 주로 서버 측에서 사용됨
 
-	if (bind(sSockFd, (struct sockaddr*)&sAddr, sAddrSize) == -1) {
-		fprintf(stderr, "bind()_errno[%d]:%s\n", errno, strerror(errno));
-		return 0;
+	//생성한 소켓을 서버 소켓으로 등록함 (성공하면 서버 소켓에 sAddr(주소 정보)가 할당됨)
+	Bind = bind(sSockFd, (struct sockaddr*)&sAddr, sAddrSize);
+	if (Bind == -1) {
+		fprintf(stderr, "bind|errno[%d]", errno);
+		goto EXIT;
 	}
 
-	if (listen(sSockFd, MAXPENDING) == -1) {
-		fprintf(stderr, "listen()_errno[%d]:%s\n", errno, strerror(errno));
-		return 0;
+	//서버 소켓을 통해 클라이언트의 접속 요청을 확인하도록 설정함
+	if (listen(sSockFd, MAX_PENDING) == -1) {
+		fprintf(stderr, "listen|errno[%d]", errno);
+		goto EXIT;
 	}
-	
-	printf("waiting connection from client\n");
+
+	//클라이언트 접속 요청 대기 및 허락함 & 클라이언트와 통신을 위해 새 소켓 생성함
 	if ((cSockFd = accept(sSockFd, (struct sockaddr*)&cAddr, &cAddrSize)) == -1) {
-		fprintf(stderr, "accept/errno[%d]\n", errno);
-		return 0;
-	}	
-	printf("connected with client\n");
-
-	read(cSockFd, )
-/*
-	FILE* fp = NULL;
-	fp = fopen("./address_tcp.txt", "a");
-	if (fp == NULL) {
-		fprintf(stderr, "fopen/errno[%d]\n", errno);
-		//return
+		fprintf(stderr, "accept|errno[%d]", errno);
+		goto EXIT;
 	}
 
-	if (fwrite(&data, sizeof(data), 1, fp) != 1) {
-		fprintf(stderr, "fwrite/errno[%d]\n", errno);
+	ssize_t rd;
+
+	while (1) {
+		memset(&data, 0, sizeof(data));
+
+		rd = read(cSockFd, &data, sizeof(data));
+		printf("rd[%ld]\n", rd);
+
+		if (rd == -1) {
+			fprintf(stderr, "read|errno[%d]", errno);
+			goto EXIT;
+		}
+		else if (rd != 0) {
+			FILE* fp = NULL;
+			fp = fopen("./address_tcp.txt", "a");
+			if (fp == NULL) {
+				fprintf(stderr, "fopen|errno[%d]", errno);
+				goto EXIT;
+			}
+
+			if (fwrite(&data, sizeof(data), 1, fp) != 1) {
+				fprintf(stderr, "fwrite|errno[%d]", errno);
+			}
+
+			if (fclose(fp) != 0) {
+				fprintf(stderr, "fclose|errno[%d]", errno);
+				goto EXIT;
+			}
+		}
 	}
 
-	if (fclose(fp) != 0) {
-		fprintf(stderr, "fclose/errno[%d]\n", errno)l
-		//return
+EXIT:
+	if (shutdown(cSockFd, SHUT_RDWR) == -1) {
+		fprintf(stderr, "shutdown|errno[%d]", errno);
 	}
-*/
 
-	close(cSockFd);
-	close(sSockFd);
+	if (close(cSockFd) == -1) {
+		fprintf(stderr, "close|errno[%d]", errno);
+	}
 
 	return 0;
 }
