72031657b347265f887a3287fbed258413259fe5
dhkim <dhkim@telcoware.com>
2022-07-01 13:56:07 +0900

    4-12

--- 4-12/epoll_tcp_client.c
+++ 4-12/epoll_tcp_client.c
@@ -9,6 +9,9 @@
 #include <errno.h>
 #include <arpa/inet.h>
 
+#define WRITE_FAIL -1
+#define WRITE_EOF 0
+
 typedef struct _AddrBook {
 	char szName[13];
 	char szPhone[14];
@@ -17,36 +20,42 @@ typedef struct _AddrBook {
 
 const char* szExit = "exit";
 void ClearStdin(char* c);
+//int MyWrite(int fd, void* buf);
 
 int main(int argc, char* argv[])
 {
-	if (argc != 2) {
+	if (argc != 2)
+	{
 		printf("하나의 파일명을 입력해주세요.\n");
 		return 0;
 	}
 
-	int nPort;
+	int nPort = 0;
+	int nFd = 0;
 	char szFileName[255];
-	//TODO memset 
-	int nFd;
 
-	if (sizeof(argv[1]) > sizeof(szFileName)) {
+	if (sizeof(argv[1]) > sizeof(szFileName))
+	{
 		printf("파일명이 너무 깁니다.\n");
 		return 0;
 	}
 
 	nPort = 7000;
-	strncpy(szFileName, argv[1], sizeof(argv[1]));
-	//TODO NULL
 	nFd = 0;
+	memset(&szFileName, 0, sizeof(szFileName));
+	strncpy(szFileName, argv[1], sizeof(argv[1]));
+	szFileName[sizeof(szFileName) - 1] = '\0';
 	
 	struct sockaddr_in tAddr;
+
 	socklen_t nAddrSize;
 	nAddrSize = sizeof(tAddr);
+	
 	ADDRBOOK tAddrBook;
 	
 	nFd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (nFd == -1) {
+	if (nFd == -1)
+	{
 		fprintf(stderr, "socket|errno[%d]\n", errno);
 		return 0;
 	}
@@ -54,17 +63,20 @@ int main(int argc, char* argv[])
 	memset(&tAddr, 0, nAddrSize);
 	tAddr.sin_family = AF_INET;
 	tAddr.sin_port = htons(nPort);
-	if (tAddr.sin_port == -1) {
+	if (tAddr.sin_port == -1)
+	{
 		fprintf(stderr, "htons|errno[%d]\n", errno);
 		goto EXIT;
 	}
 	tAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	if (tAddr.sin_addr.s_addr == -1) {
+	if (tAddr.sin_addr.s_addr == -1)
+	{
 		fprintf(stderr, "htonl|errno[%d]\n", errno);
 		goto EXIT;
 	}
 
-	if (connect(nFd, (struct sockaddr*)&tAddr, nAddrSize) == -1) {
+	if (connect(nFd, (struct sockaddr*)&tAddr, nAddrSize) == -1)
+	{
 		fprintf(stderr, "connect|errno[%d]\n", errno);
 		goto EXIT;
 	}
@@ -72,78 +84,91 @@ int main(int argc, char* argv[])
 
 	int nWrite = 0;
 	nWrite = write(nFd, &szFileName, sizeof(szFileName));
-		if (nWrite == -1) {
-			fprintf(stderr, "write|errno[%d]\n", errno);
-			goto EXIT;
-		}
-	
-	while(1) {
-		memset(&tAddrBook, 0, sizeof(tAddrBook));
-	
-		int nTotalWrite = 0;
+	if (nWrite == -1)
+	{
+		fprintf(stderr, "write|errno[%d]\n", errno);
+		goto EXIT;
+	}
+	printf("파일명 전송[%s]\n", szFileName);
+	//MyWrite(nFd, &szFileName);
 
-		//TODO whilex
-	/*	nWrite = write(nFd, &szFileName, sizeof(szFileName));
-		if (nWrite == -1) {
-			fprintf(stderr, "write|errno[%d]\n", errno);
-			goto EXIT;
-		}
-*/
+	while(1)
+	{
 		do {
+			memset(&tAddrBook, 0, sizeof(tAddrBook));
+
 			printf("Name: ");
-			if (fgets(tAddrBook.szName, sizeof(tAddrBook.szName), stdin) == NULL) {
+			if (fgets(tAddrBook.szName, sizeof(tAddrBook.szName), stdin) == NULL)
+			{
 				fprintf(stderr, "fgets|errno[%d]\n", errno);
 				goto EXIT;
 			}
 			ClearStdin(tAddrBook.szName);
 			
-			if (strcmp(szExit, tAddrBook.szName) == 0) {
-				nWrite = write(nFd, &tAddrBook.szName, sizeof(tAddrBook.szName));
-				if (nWrite == -1) {
+			if (strcmp(szExit, tAddrBook.szName) == 0)
+			{
+				nWrite = write(nFd, &tAddrBook, sizeof(tAddrBook));
+				if (nWrite == -1)
+				{
 					fprintf(stderr, "write|errno[%d]\n", errno);
 				}
+
+				//MyWrite(nFd, &tAddrBook.szName);
 				goto EXIT;
 			}
 
 			printf("Phone Num: ");
-			if (fgets(tAddrBook.szPhone, sizeof(tAddrBook.szPhone), stdin) == NULL) {
+			if (fgets(tAddrBook.szPhone, sizeof(tAddrBook.szPhone), stdin) == NULL)
+			{
 				fprintf(stderr, "fgets|errno[%d]\n", errno);
 				goto EXIT;
 			}
 			ClearStdin(tAddrBook.szPhone);
 			
-			if ((tAddrBook.szPhone[3] != '-') || (tAddrBook.szPhone[8] != '-')) {
+			if ((tAddrBook.szPhone[3] != '-') || (tAddrBook.szPhone[8] != '-'))
+			{
 				printf("전화번호를 xxx-xxxx-xxxx 형태로 입력해주세요.\n");
-				printf("처음으로 돌아갑니다.\n");
 			}
 		} while ((tAddrBook.szPhone[3] != '-') || (tAddrBook.szPhone[8] != '-'));
 
 		printf("Address: ");
-		if (fgets(tAddrBook.szAddress, sizeof(tAddrBook.szAddress), stdin) == NULL) {
+		if (fgets(tAddrBook.szAddress, sizeof(tAddrBook.szAddress), stdin) == NULL)
+		{
 			fprintf(stderr, "fgets|errno[%d]\n", errno);
 			goto EXIT;
 		}
 		ClearStdin(tAddrBook.szAddress);
 
-		while (1) {
+/*
+		if (MyWrite(nFd, &tAddrBook) == WRITE_FAIL)
+		{
+			goto EXIT;
+		}
+*/
+		int nTotalWrite = 0;
+
+		while (1)
+		{
 			nWrite = write(nFd, &tAddrBook + nTotalWrite, sizeof(tAddrBook) - nTotalWrite);
-			if (nWrite == -1) {
+			if (nWrite == -1)
+			{
 				fprintf(stderr, "write|errno[%d]\n", errno);
 				goto EXIT;
 			}
-			printf("nWrite[%d]\n", nWrite);
-			
+			printf("nWrite[%d] ", nWrite);
+		
 			nTotalWrite += nWrite;
-			if (nTotalWrite == sizeof(tAddrBook)) {
-				printf("nTotalWrite[%d]:데이터 송신\n", nTotalWrite);
+			if (nTotalWrite == sizeof(tAddrBook))
+			{
+				printf("nTotalWrite[%d]\n", nTotalWrite);
 				break;
 			}
-		}
-		printf("test\n");
+		} 
 	}
 
 EXIT:
-	if (close(nFd) == -1) {
+	if (close(nFd) == -1)
+	{
 		fprintf(stderr, "close|errno[%d]\n", errno);
 	}
 
@@ -152,13 +177,45 @@ EXIT:
 
 void ClearStdin(char* c)
 {
-	if (c == NULL) {
+	if (c == NULL)
+	{
 		return;
 	}
 
-	if (c[strlen(c) - 1] == '\n') {
+	if (c[strlen(c) - 1] == '\n')
+	{
 		c[strlen(c) - 1] = '\0';
 	}
 
 	__fpurge(stdin);
 }
+
+/*
+int MyWrite(int fd, void* buf)
+{
+	int nWrite = 0;
+	int nTotalWrite = 0;
+
+	while (1)
+	{
+		nWrite = write(fd, buf + nTotalWrite, sizeof(buf) - nTotalWrite);
+		if (nWrite == -1)
+		{
+			fprintf(stderr, "write|errno[%d]\n", errno);
+			return WRITE_FAIL;
+		}
+		printf("nWrite[%d]\n", nWrite);
+		
+		nTotalWrite += nWrite;
+		if (nTotalWrite == sizeof(buf))
+		{
+			printf("nTotalWrite[%d]:데이터 송신\n", nTotalWrite);
+			return nTotalWrite;
+		}
+		else if (nTotalWrite == 0)
+		{
+			return WRITE_EOF;
+		}
+	} 
+}
+*/
--- 4-12/epoll_tcp_server.c
+++ 4-12/epoll_tcp_server.c
@@ -10,23 +10,31 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 
-typedef struct _AddrBook {
+#define READ_FAIL -1
+#define READ_EOF 0
+
+typedef struct _AddrBook
+{
 	char szName[13];
 	char szPhone[14];
 	char szAddress[151];
 } ADDRBOOK;
 
-typedef struct _Client {
+typedef struct _Client
+{
 	int nFd;
 	int nReceive;
+	char szFilePath[261];
 } CLIENT;
 
 const char* szExit = "exit";
+//int MyRead(int fd, void* buf);
 
 int main()
 {
 	int i = 0;
 	int j = 0;
+	int k = 0;
 	int nIndex = 0;
 	int nPort = 7000;
 	int nMaxPending = 5;
@@ -36,207 +44,298 @@ int main()
 	int nEpollFd = 0;
 	int nFdCount = 0;
 	int nTimeout = -1;
-	int nAccept = 0;
-	char szFileName[255]; //filename
-	char szBuffer[261]; //./filename.txt
-//TODO size
+	int nClient = 0;
+	int nRead = 0;
+	int nTotalRead = 0;
+	char szFileName[255];
+
 	struct sockaddr_in tAddr;
 	struct sockaddr_in tClientAddr;
 	socklen_t nAddrSize;
 	socklen_t nClientAddrSize;
-
+	
 	nAddrSize = sizeof(tAddr);
 	nClientAddrSize = sizeof(tClientAddr);
 	memset(&tAddr, 0, nAddrSize);
 	memset(&tClientAddr, 0, nClientAddrSize);
 
-	struct epoll_event tEvent; //서버쪽 소켓에  epoll 세팅 구조체
-	struct epoll_event* ptEvents; // 클라이언트 소켓들을 관리하기 위한 구조체
-
-	ptEvents = (struct epoll_event*)malloc(sizeof(struct epoll_event) * nMaxConnecting);
-	if (ptEvents == NULL) {
+	ADDRBOOK tAddrBook;
+	CLIENT* tClient;
+	tClient = (CLIENT*)malloc(sizeof(CLIENT) * nMaxConnecting);
+	if (tClient == NULL)
+	{
 		fprintf(stderr, "malloc|errno[%d]\n", errno);
 		return 0;
 	}
 
-	ADDRBOOK tAddrBook;
-	CLIENT* tClient;
-
-	int nRead = 0;
-	int nTotalRead = 0;
+	struct epoll_event tEvent;
+	struct epoll_event* ptEvents;
+	ptEvents = (struct epoll_event*)malloc(sizeof(struct epoll_event) * nMaxConnecting);
+	if (ptEvents == NULL)
+	{
+		fprintf(stderr, "malloc|errno[%d]\n", errno);
+		return 0;
+	}
+	
 	FILE* pFile = NULL;
-	memset(&szFileName, 0, sizeof(szFileName));
-	memset(&szBuffer, 0, sizeof(szBuffer));
 
 	nListenFd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (nListenFd == -1) {
+	if (nListenFd == -1)
+	{
 		fprintf(stderr, "socket|errno[%d]\n", errno);
 		return 0;
 	}
 	
 	nEpollFd = epoll_create(100);
-	if (nEpollFd == -1) {
+	if (nEpollFd == -1)
+	{
 		fprintf(stderr, "epoll_create|errno[%d]\n", errno);
 		goto EXIT;
 	}
 
 	tAddr.sin_family = AF_INET;
 	tAddr.sin_port = htons(nPort);
-	if (tAddr.sin_port == -1) {
+	if (tAddr.sin_port == -1)
+	{
 		fprintf(stderr, "htons|errno[%d]\n", errno);
 		goto EXIT;
 	}
 	tAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	if (tAddr.sin_addr.s_addr == -1) {
+	if (tAddr.sin_addr.s_addr == -1)
+	{
 		fprintf(stderr, "htonl|errno[%d]\n", errno);
 		goto EXIT;
 	}
 	
 	const int flag = 1;
-	if (setsockopt(nListenFd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(int)) == -1) {
+	if (setsockopt(nListenFd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(int)) == -1)
+	{
 		fprintf(stderr, "setsockopt|errno[%d]\n", errno);
 		goto EXIT;
 	}
 
-	if (bind(nListenFd, (struct sockaddr*)&tAddr, nAddrSize) == -1) {
+	if (bind(nListenFd, (struct sockaddr*)&tAddr, nAddrSize) == -1)
+	{
 		fprintf(stderr, "bind|errno[%d]\n", errno);
 		goto EXIT;
 	}
 	
-	if (listen(nListenFd, nMaxPending) == -1) {
+	if (listen(nListenFd, nMaxPending) == -1)
+	{
 		fprintf(stderr, "listen|errno[%d]\n", errno);
 		goto EXIT;
 	}
 
 	tEvent.events = EPOLLIN;
 	tEvent.data.fd = nListenFd;
-	if (epoll_ctl(nEpollFd, EPOLL_CTL_ADD, nListenFd, &tEvent) == -1) {
+	if (epoll_ctl(nEpollFd, EPOLL_CTL_ADD, nListenFd, &tEvent) == -1)
+	{
 		fprintf(stderr, "epoll_ctl|errno[%d]\n", errno);
 		goto EXIT;
 	}
 
-	while (1) {
-		printf("1\n");
+	while (1)
+	{
 		nFdCount = epoll_wait(nEpollFd, ptEvents, nMaxConnecting, nTimeout);
-		if (nFdCount == -1) {
+		if (nFdCount == -1)
+		{
 			fprintf(stderr, "epoll_wait|errno[%d]\n", errno);
 			goto EXIT;
 		}
 
-		for (i = 0; i < nFdCount; i++) {
-			if (ptEvents[i].events & EPOLLIN) { //TODO events가 EPOLLIN인지 EPOLLERR인지 확인
-				if (ptEvents[i].data.fd == nListenFd) {					
+		for (i = 0; i < nFdCount; i++)
+		{
+			if (ptEvents[i].events & EPOLLIN)
+			{
+				if (ptEvents[i].data.fd == nListenFd)
+				{
 					nClientFd = accept(nListenFd, (struct sockaddr*)&tClientAddr, &nClientAddrSize);
-					if (nClientFd == -1) {
+					if (nClientFd == -1)
+					{
 						fprintf(stderr, "accept|errno[%d]\n", errno);
 						goto EXIT;
 					}
-					printf("Accept: nClientFd[%d]\n", nClientFd);
 
-//TODO nAccept 5 close(fd) nClient
+					//TODO 5개 초과되는 연결 요청에 대해서는 강제로 끊음
+					if (nClient == 5) {
+						if (close(nClientFd) == -1)
+						{
+							fprintf(stderr, "close|errno[%d]\n", errno);
+							goto EXIT;
+						}
+						printf("Client 동시 접속 수가 5개를 초과하여 연결을 끊습니다.\n");
+						goto WHILE;
+					}
 
-					tClient = (struct _Client*)malloc(sizeof(struct _Client));
-					tClient->nFd = nClientFd;
+					for (k =  0; k < nMaxConnecting; k++)
+					{
+						if (tClient[k].nFd == 0)
+						{
+							tClient[k].nFd = nClientFd;
+							nIndex = k;
+							break;
+						}
+					}
 
 					tEvent.events = EPOLLIN;
-					tEvent.data.ptr = tClient;
-//TODO tEvent.data.fd = tClient->nFd;
-					if (epoll_ctl(nEpollFd, EPOLL_CTL_ADD, nClientFd, &tEvent) == -1) {
+					//tEvent.data = tClient[nIndex];
+					tEvent.data.fd = tClient[nIndex].nFd;
+					
+					if (epoll_ctl(nEpollFd, EPOLL_CTL_ADD, tClient[nIndex].nFd, &tEvent) == -1)
+					{
 						fprintf(stderr, "epoll_ctl|errno[%d]\n", errno);
 						goto EXIT;
 					}
+
+					nClient++;
+					printf("=====================================\n동시 접속 중인 Client 수: %d\n=====================================\n", nClient);
 				}
-				else {
-					tClient = ptEvents[i].data.ptr;
-
-					if (tClient->nReceive == 0) {
-						nRead = read(tClient->nFd, &szFileName, sizeof(szFileName));
-						if (nRead == -1) {
-							fprintf(stderr, "read|errno[%d]\n", errno);
-							goto EXIT;
-						}
-						snprintf(szBuffer, sizeof(szBuffer), "./%s.txt", szFileName);
-
-						tClient->nReceive = 1;
-						printf("=====================================\n변화 감지된 Client: nFd[%d]\n파일명 전송받음: nReceive[%d]\n파일 경로: szBuffer[%s]\n=====================================\n", tClient->nFd, tClient->nReceive, szBuffer);
-						goto CONTINUE; //TODO continue
-					}
-					else if (tClient->nReceive == 1)
+				else
+				{
+					for (j = 0; j < nClient; j++)
 					{
-						while (1)
+						if (ptEvents[i].data.fd == tClient[j].nFd)
 						{
-							//TODO init -> 함수로
-							nRead = read(tClient->nFd, &tAddrBook + nTotalRead, sizeof(tAddrBook) - nTotalRead); //TODO totalread
+							//tClient[j] = ptEvents[i].data.ptr;
+							tClient[j].nFd = ptEvents[i].data.fd;
 							
-							if (nRead == -1)
-							{
-								fprintf(stderr, "read|errno[%d]\n", errno);
-								goto EXIT;
-							}
+							if (tClient[j].nReceive == 0) {
+								memset(&szFileName, 0, sizeof(szFileName));
+								memset(&tClient[j].szFilePath, 0, sizeof(tClient[j].szFilePath));
+								
+								//TODO READ 함수
+								//MyRead(tClient->nFd, &szFileName);
 
-							if (strcmp(szExit, tAddrBook.szName) == 0)
-							{
-								printf("=====================================\n변화 감지된 Client: nFd[%d]\nexit를 전송받음: nReceive[%d]\n관찰대상에서 삭제\n=====================================\n", tClient->nFd, tClient->nReceive);
-
-								if (epoll_ctl(nEpollFd, EPOLL_CTL_DEL, tClient->nFd, NULL) == -1)
+								nRead = read(tClient[j].nFd, &szFileName, sizeof(szFileName));
+								if (nRead == -1)
 								{
-									fprintf(stderr, "epoll_ctl|errno[%d]\n", errno);
-								}
-								if (close(tClient->nFd) == -1)
-								{
-									fprintf(stderr, "close|errno[%d]\n", errno);
+									fprintf(stderr, "read|errno[%d]\n", errno);
 									goto EXIT;
 								}
-								
-								free(tClient);
-								//TODO nAccpet -1
-								//	goto CONTINUE;
-							}
+								snprintf(tClient[j].szFilePath, sizeof(tClient[j].szFilePath), "./%s.txt", szFileName);
 
-							nTotalRead += nRead;
-							
-							if (nTotalRead == sizeof(tAddrBook))
+								tClient[j].nReceive = 1;
+								printf("=====================================\n변화 감지된 Client: #%d\n전송받은 파일 경로: %s\n=====================================\n", tClient[j].nFd, tClient[j].szFilePath);
+								continue;
+							}
+							else if (tClient[j].nReceive == 1)
 							{
-								printf("=====================================\n변화 감지된 Client: nFd[%d]\n주소록 정보 전송받음: nTotalRead[%d]\n=====================================\n", tClient->nFd, nTotalRead);
-								goto WRITE;
-							}
-						}
-WRITE:
-						pFile = fopen(szBuffer, "a");
-						if (pFile == NULL) {
-							fprintf(stderr, "fopen|errno[%d]\n", errno);
-							goto EXIT;
-						}
-						if (fwrite(&tAddrBook, sizeof(tAddrBook), 1, pFile) != 1) {
-							fprintf(stderr, "fwrite|errno[%d]\n", errno);
-						}
-						if (fclose(pFile) != 0) {
-							fprintf(stderr, "fclose|errno[%d]\n", errno);
-							goto EXIT;
-						}
+								//MyRead(tClient->nFd, &tAddrBook);
+								nRead = 0;
+								nTotalRead = 0;
+								while (1)
+								{
+									nRead = read(tClient[j].nFd, &tAddrBook + nTotalRead, sizeof(tAddrBook) - nTotalRead);	
+									if (nRead == -1)
+									{
+										fprintf(stderr, "read|errno[%d]\n", errno);
+										goto EXIT;
+									}
 
-						goto CONTINUE;
-					}
-				}
-			}
-			else if (ptEvents[i].events & EPOLLERR) {
+									nTotalRead += nRead;		
+									if (nTotalRead == sizeof(tAddrBook))
+									{
+										printf("=====================================\n변화 감지된 Client: #%d\n주소록 정보 바이트 수: %d\n=====================================\n", tClient[j].nFd, nTotalRead);
+										break;
+									}
+								}
+							
+								if (strcmp(szExit, tAddrBook.szName) == 0)
+								{
+									printf("=====================================\n변화 감지된 Client: #%d\n관찰대상에서 삭제\n=====================================\n", tClient[j].nFd);
+
+									if (epoll_ctl(nEpollFd, EPOLL_CTL_DEL, tClient[j].nFd, NULL) == -1)
+									{
+										fprintf(stderr, "epoll_ctl|errno[%d]\n", errno);
+									}
+									if (close(tClient[j].nFd) == -1)
+									{
+										fprintf(stderr, "close|errno[%d]\n", errno);
+										goto EXIT;
+									}
+
+									//TODO 클라이언트 연결 종료 시 처리
+									nClient--;
+									memset(&tClient[j].nFd, 0, sizeof(tClient[j].nFd));
+									memset(&tClient[j].nReceive, 0, sizeof(tClient[j].nReceive));
+									memset(&tClient[j].szFilePath, 0, sizeof(tClient[j].szFilePath));	
+									printf("=====================================\n동시 접속 중인 Client 수: %d\n=====================================\n", nClient);
+
+									goto CONT;
+								}
+
+								pFile = fopen(tClient[j].szFilePath, "a");
+								if (pFile == NULL)
+								{
+									fprintf(stderr, "fopen|errno[%d]\n", errno);
+									goto EXIT;
+								}
+								if (fwrite(&tAddrBook, sizeof(tAddrBook), 1, pFile) != 1)
+								{
+									fprintf(stderr, "fwrite|errno[%d]\n", errno);
+								}
+								if (fclose(pFile) != 0)
+								{
+									fprintf(stderr, "fclose|errno[%d]\n", errno);
+									goto EXIT;
+								}
+
+
+							}//else if (tClient->nReceive == 1)
+						}//if (tClient[j]->nFd == ptEvents[i].data.fd)
+					}//for (j = 0; j < nClient; j++)
+CONT:
+					printf("goto CONT\n");
+
+				}//else
+			}//if EPOLLIN
+			else if (ptEvents[i].events & EPOLLERR)
+			{
 				fprintf(stderr, "EPOLLERR|errno[%d]\n", errno);
-				goto EXIT; //TODO close client  4 -> nAccept
+				goto EXIT;
 			}
-		}
+		}//for(i = 0; i < nFdCount; i++)
 
-CONTINUE:
-	printf("\n");
-	
-	}
+WHILE:
+		printf("goto WHILE\n");
+
+	}//while(1)
 
 EXIT:
 	free(tClient);
 	free(ptEvents);
 
-	if (close(nListenFd) == -1) {
+	if (close(nListenFd) == -1)
+	{
 		fprintf(stderr, "close|errno[%d]\n", errno);
 	}
-	
+
 	return 0;
 }
+
+/*int MyRead(int fd, void* buf)
+{
+  int nRead = 0;
+  int nTotalRead = 0;
+
+  while (1)
+  {
+  nRead = read(fd, buf + nTotalRead, sizeof(buf) - nTotalRead);
+  if (nRead == -1)
+  {
+  fprintf(stderr, "read|errno[%d]\n", errno);
+  return READ_FAIL;
+  }
+
+  nTotalRead += nRead;
+  if (nTotalRead == sizeof(buf))
+  {
+  printf("=====================================\n변화 감지된 Client: nFd[%d]\nnTotalRead[%d]\n=====================================\n", fd, nTotalRead);
+  return nTotalRead;
+  }
+  else if (nTotalRead == 0)
+  {
+  return READ_EOF;
+  }
+  }
+}*/
